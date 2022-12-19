d27e874a0748a437725733555bdc79b65e63e9d0
Alekseii Guskov <ingrzhd@gmail.com>
2022-12-19 16:40:02 +0300

    Created this project with maven

---
+++ src/main/java/com/epam/training/student_Aleksei_Guskov/App.java
@@ -0,0 +1,13 @@
+package com.epam.training.student_Aleksei_Guskov;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
