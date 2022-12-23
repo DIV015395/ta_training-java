959cc96125a5a4fcfdacb5bd83d6c3398f9c90bf
Alekseii Guskov <ingrzhd@gmail.com>
2022-12-23 19:38:52 +0300

    completed all module

---
+++ src/main/java/com/epam/training/student_Aleksei_Guskov/fundamental/java_basics/learn_3/QuadraticEquation.java
@@ -0,0 +1,31 @@
+package com.epam.training.student_Aleksei_Guskov.fundamental.java_basics.learn_3;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import static java.lang.Math.sqrt;
+
+public class QuadraticEquation {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        double a = scanner.nextDouble();
+        double b = scanner.nextDouble();
+        double c = scanner.nextDouble();
+        double discriminant = b*b-4*a*c;
+        if (discriminant>0) {
+            double x1 = (-b + sqrt(discriminant))/(2*a);
+            double x2 = (-b - sqrt(discriminant))/(2*a);
+            System.out.println(x1 +" "+ x2);
+        } else if (discriminant==0) {
+            double x = -b/(a*2);
+            if (x==-0.0) {
+                System.out.println("0.0");
+            } else {
+                System.out.println(x);
+            }
+        } else {
+            System.out.println("no roots");
+        }
+    }
+
+}
